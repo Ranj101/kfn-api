212ff81600718738d19c8de0c706a1c3794da455
ranj rashid <ranjrashid01@gmail.com>
2023-03-10 22:57:57 +0300

    adds user auth context tracker service

---
+++ src/KfnApi/Abstractions/IAuthContext.cs
@@ -0,0 +1,16 @@
+﻿using KfnApi.Helpers.Authorization;
+using KfnApi.Models.Entities;
+
+namespace KfnApi.Abstractions;
+
+public interface IAuthContext
+{
+    bool HasUser();
+    string GetUserId();
+    User GetUser();
+    bool IsAdmin();
+    bool IsSuperAdmin();
+    bool IsSystemAdmin();
+    HashSet<Permission> GetPermissions();
+    void SetUser(User user);
+}
---
+++ src/KfnApi/Services/AuthContext.cs
@@ -0,0 +1,54 @@
+﻿using KfnApi.Abstractions;
+using KfnApi.Helpers.Authorization;
+using KfnApi.Models.Entities;
+
+namespace KfnApi.Services;
+
+public class AuthContext : IAuthContext
+{
+    private readonly IRoleMap _roleMap;
+
+    private User? _user;
+    private bool _isSuperAdmin;
+    private bool _isSystemAdmin;
+    private HashSet<Permission> _permissions = new();
+
+    public AuthContext(IRoleMap roleMap)
+    {
+        _roleMap = roleMap;
+    }
+
+    public bool HasUser()
+        => _user is not null;
+
+    public string GetUserId()
+        => _user!.Id;
+
+    public User GetUser()
+        => _user!;
+
+    public bool IsAdmin()
+        => _isSuperAdmin || _isSystemAdmin;
+
+    public bool IsSuperAdmin()
+        => _isSuperAdmin;
+
+    public bool IsSystemAdmin()
+        => _isSystemAdmin;
+
+    public HashSet<Permission> GetPermissions()
+        => _permissions;
+
+    public void SetUser(User user)
+    {
+        _user = user;
+
+        _isSuperAdmin = user.Roles.Contains(Roles.SuperAdmin);
+        _isSystemAdmin = user.Roles.Contains(Roles.SystemAdmin);
+
+        _permissions = _roleMap.GetRoleDefinitions()
+            .Where(kvp => user.Roles.Contains(kvp.Key))
+            .SelectMany(kvp => kvp.Value)
+            .ToHashSet();
+    }
+}
