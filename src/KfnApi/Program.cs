7c5d784b061f6d0c05f1f01d3291b8ef00a2e40f
ranj rashid <ranjrashid01@gmail.com>
2023-03-11 23:17:42 +0300

    adds database migration

--- src/KfnApi/Program.cs
+++ src/KfnApi/Program.cs
@@ -1,8 +1,31 @@
 using KfnApi.Configurations.StartupConfigurations;
+using KfnApi.Helpers.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
+
+if (args.Contains("--migrate"))
+{
+    builder.Services.ConfigureDatabase(builder.Configuration);
+    var migrationRunner = builder.Build();
+    var logger = migrationRunner.Logger;
+    logger.LogInformation("migration initiated");
+    try
+    {
+        await using var scope = migrationRunner.Services.CreateAsyncScope();
+        var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        await db.Database.MigrateAsync();
+        logger.LogInformation("migration successful");
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "migration failed");
+    }
+
+    return;
+}
 
 services.AddControllers();
 services.AddEndpointsApiExplorer();
