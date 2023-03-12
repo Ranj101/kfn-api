446dc2189c3ec291f14aceb331221cd1fde1c2e7
ranj rashid <ranjrashid01@gmail.com>
2023-03-12 12:15:34 +0300

    updates remote user service

--- src/KfnApi/Services/RemoteUserService.cs
+++ src/KfnApi/Services/RemoteUserService.cs
@@ -10,7 +10,7 @@ public class RemoteUserService : IRemoteUserService
     private readonly HttpClient _httpClient;
 
     private const string GetByIdEndpoint = "users/{0}";
-
+    private const string GetRoleByIdEndpoint = "users/{0}/roles";
     public RemoteUserService(IHttpClientFactory clientFactory)
     {
         _httpClient = clientFactory.CreateClient(AuthDefaults.IdentityWrapperClient);
@@ -18,13 +18,21 @@ public class RemoteUserService : IRemoteUserService
 
     public async Task<AuthUser?> GetByIdAsync(string id, CancellationToken ct = default)
     {
-        using var response = await _httpClient.GetAsync(string.Format(GetByIdEndpoint, id), ct);
-        return await DeserializeJsonAsync(await response.Content.ReadAsStreamAsync(ct), ct);
+        using var userResponse = await _httpClient.GetAsync(string.Format(GetByIdEndpoint, id), ct);
+        var authUser = await DeserializeJsonAsync<AuthUser>(await userResponse.Content.ReadAsStreamAsync(ct), ct);
+
+        using var roleResponse = await _httpClient.GetAsync(string.Format(GetRoleByIdEndpoint, id), ct);
+        var userRoles = await DeserializeJsonAsync<List<AuthUserRole>>(await roleResponse.Content.ReadAsStreamAsync(ct), ct);
+
+        if (authUser != null)
+            authUser.Roles = userRoles?.Select(r => r.Name).ToList() ?? new ();
+
+        return authUser;
     }
 
-    private static Task<AuthUser?> DeserializeJsonAsync(Stream stream, CancellationToken ct)
+    private static Task<T?> DeserializeJsonAsync<T>(Stream stream, CancellationToken ct)
     {
-        return JsonSerializer.DeserializeAsync<AuthUser>(stream, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }, ct)
+        return JsonSerializer.DeserializeAsync<T>(stream, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }, ct)
             .AsTask();
     }
 }
