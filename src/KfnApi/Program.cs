b4690aad827b1b1c8524b11e7417c66b722c0c11
ranj rashid <ranjrashid01@gmail.com>
2023-03-13 10:45:49 +0300

    updates dockerfile

--- src/KfnApi/Program.cs
+++ src/KfnApi/Program.cs
@@ -51,14 +51,14 @@ services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseCors();
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
