7b734c4d46095e9c32e8c31e310ced59ce8b1d30
ranj rashid <ranjrashid01@gmail.com>
2023-03-23 22:36:25 +0300

    adds controller result pattern

---
+++ src/KfnApi/Helpers/KfnControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using KfnApi.Models.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KfnApi.Helpers;
+
+public class KfnControllerBase : ControllerBase
+{
+    protected IActionResult Failure(Error failure)
+    {
+        return new ObjectResult(failure)
+        {
+            StatusCode = failure.HttpCode
+        };
+    }
+
+    protected IActionResult Success(object? value, int httpCode)
+    {
+        return new ObjectResult(value)
+        {
+            StatusCode = httpCode
+        };
+    }
+}
---
+++ src/KfnApi/Models/Common/Result.cs
@@ -0,0 +1,46 @@
+﻿namespace KfnApi.Models.Common;
+
+public class Result<T> where T : class
+{
+    public Result(T value, int httpCode)
+    {
+        Value = value;
+        HttpCode = httpCode;
+    }
+
+    public Result(Error error)
+    {
+        Error = error;
+    }
+
+    public T? Value { get; }
+    public int HttpCode { get; }
+    public Error? Error { get; set; }
+
+    public static Result<T> SuccessResult(T value, int httpCode)
+    {
+        return new Result<T>(value, httpCode);
+    }
+
+    public static Result<T> FailureResult(Error error)
+    {
+        return new Result<T>(error);
+    }
+
+    public bool IsSuccess()
+    {
+        return Value != null;
+    }
+
+    public bool IsFailure()
+    {
+        return Error != null;
+    }
+}
+
+public class Error
+{
+    public int HttpCode { get; set; }
+    public string? Title { get; set; }
+    public string? Detail { get; set; }
+}
